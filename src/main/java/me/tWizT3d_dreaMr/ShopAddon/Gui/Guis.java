336cdcd488d2ef5c4d859ed5f64d2a906dae31ee
tWizT3ddreaMr <tWizt3d.dreamr0729@gmail.com>
2025-05-02 21:51:06 -0500

    Revert "Enhanced Search, Filtering and GUI"

--- src/main/java/me/tWizT3d_dreaMr/ShopAddon/Gui/Guis.java
+++ src/main/java/me/tWizT3d_dreaMr/ShopAddon/Gui/Guis.java
@@ -9,35 +9,31 @@ import com.snowgears.shop.shop.AbstractShop;
 import com.snowgears.shop.util.CurrencyType;
 
 public class Guis {
-	public static ArrayList<Gui> guilist;
-	public static CurrencyType type;
-
-	public static Gui getGui(InventoryView IV) {
-		if (guilist == null) {
-			guilist = new ArrayList<Gui>();
-		}
-		if (guilist.isEmpty()) {
-			return null;
-		}
-		for (Gui gui : guilist) {
-			if (gui.is(IV))
-				return gui;
-		}
+public static ArrayList<Gui> guilist;
+public static CurrencyType type;
+public static Gui getGui(InventoryView IV) {
+	if(guilist==null) {
+		guilist= new ArrayList<Gui>();
+	}
+	if(guilist.isEmpty()) {
 		return null;
 	}
-
-	public static void add(Gui gui) {
-		if (guilist == null) {
-			guilist = new ArrayList<Gui>();
-		}
-		guilist.add(gui);
+	for(Gui gui:guilist) {
+		if(gui.is(IV))
+			return gui;
 	}
-
-	public static void makeGui(Player p, AbstractShop s) {
-		add(new Gui(p, s));
+	return null;
+}
+public static void add(Gui gui) {
+	if(guilist==null) {
+		guilist= new ArrayList<Gui>();
 	}
-
-	public static CurrencyType getCurrencyType() {
-		return type;
-	}
+	guilist.add(gui);
+}
+public static void makeGui(Player p, AbstractShop s) {
+	add(new Gui(p, s));
+}
+public static CurrencyType getCurrencyType() {
+	return type;
+}
 }
